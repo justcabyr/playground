11578176fdc04eb76bf61ae915d44f204f222dd4
justcabyr <justcabyr@gmail.com>
2023-10-30 22:40:38 -0230

    initial vim setup

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # vim-mode
+
+h,j,k,l for basic movement.
+w,b for word hopping. Effectively the same as Option/Ctrl + arrow keys
+yy to "copy" a line, called Yank
+dd to delete, and yank, a line
+p and P to paste the contents of the implicit register below / above
+Most of the ways to go into insert mode!
+i and a for which side of the cursor
+I and A for which side of the line
+o and O for belowe / above line
+zz - centre screen
+
+
+:set scrolloff=8
+:set number
+:set relativenumber | :set rnu
+:set norelativenumber
+
+dd6j | V6jdd 
---
+++ lessons/exercise.js
@@ -0,0 +1,53 @@
+---
+path: "/some-javascript"
+title: "some javascript"
+order: "72A"
+section: "Misc Content"
+description: "This is the first exercise to try out."
+---
+
+// Delete the description.
+// That seems slow right?
+// <count>Delete
+// try typing, with your cursor on the first line the following.
+// 7dd
+// What does that mean about j and k?
+// But we have a problem down we?
+function foo(a, b, c) {
+	switch (a) {
+		case 1: 
+			return b + c;
+			break;
+		default:
+			return a * b + c;
+	}
+
+	throw new Error("This should never happen");
+}
+
+if (true) {
+	console.log(foo(1, 5, 7));
+}
+
+if (false) {
+	console.log(foo(1, 5, 7));
+}
+
+if (false || true) {
+	console.log(foo(1, 5, 7));
+}
+
+foo(
+    foo(1),
+    foo(2),
+    foo(3))
+
+foo(
+    foo(4),
+    foo(5),
+    foo(6))
+
+foo(
+    foo(7),
+    foo(8),
+    foo(9))
